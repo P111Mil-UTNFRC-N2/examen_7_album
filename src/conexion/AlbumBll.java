9bc229a4e6ca353f61dd379460503c08e0d8c004
Agustin <Agustin@172.16.143.29>
2018-10-08 20:07:07 -0300

    Merge origin/back_end into back_end
    Conflicts:
    	nbproject/private/private.properties

--- src/conexion/AlbumBll.java
+++ src/conexion/AlbumBll.java
@@ -16,7 +16,20 @@ public class AlbumBll
     }
     private AlbumBll()
     {
-  
+        Conexion con = Conexion.getInstance();
+        
     }
+    public void agregarAlbum(int idalbum,String nombre, int cantmax, int fechacreacion)
+    {
+        String sql;
+        sql = "insert into album values("+idalbum+",'"+nombre+"',"+cantmax+", "+fechacreacion+");";
+        Conexion.getInstance().ejecutarSQL(sql);
+    }
+    public void agregarFoto(int idfoto, int idalbum, String descripcion, String nombrearchivo, int tamaño)
+    {
+        String sql;
+        sql = "insert into foto values("+ idfoto +", "+idalbum +", '"+descripcion+"', '"+nombrearchivo+"', "+tamaño+");";
+        Conexion.getInstance().ejecutarSQL(sql);
+    } 
     
 }
