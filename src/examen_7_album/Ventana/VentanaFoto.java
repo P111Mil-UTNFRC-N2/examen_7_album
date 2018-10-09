2ed6b682111cb4796c76e39a0e1d31e1982e4114
alumno <alumno@913-m11.gabinete.frc.utn.edu.ar>
2018-10-08 21:05:04 -0300

    Merge origin/master
    Conflicts:
    	111milExamen6/src/main/java/ar/edu/frc/milexamen6/ventana/Venta.java

--- src/examen_7_album/Ventana/VentanaFoto.java
+++ src/examen_7_album/Ventana/VentanaFoto.java
@@ -10,6 +10,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 /**
  *
@@ -21,6 +23,7 @@ public class VentanaFoto extends JFrame {
     public JTextField nArchivoText, tamañoText, descripcionText, personaText;
     public JButton addPersona, ok, cancelar;
     public JTable personaJTable;
+    DefaultTableModel tablam;
     public VentanaFoto(){
        
         principal=new JPanel();
@@ -48,9 +51,15 @@ public class VentanaFoto extends JFrame {
         
         addPersona=new JButton("AñadirPersona");
         addPersona.setBounds(300, 120, 150, 20);
-        
+       
+       //tabla 
         personaJTable=new JTable();
         personaJTable.setBounds(20, 150, 300, 200);
+        tablam=new DefaultTableModel();
+        tablam.addColumn("Nombre");
+        personaJTable.setModel(tablam);
+        
+        
         
         ok=new JButton("Ok");
         ok.setBounds(200, 430, 100, 20);
@@ -100,3 +109,80 @@ public class VentanaFoto extends JFrame {
     }
     
 }
+/*
+package examen;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.*;
+import java.util.Vector;
+public class TABLA extends javax.swing.JFrame {
+double contotal=0;
+DefaultTableModel tablam = new DefaultTableModel();
+public TABLA() {
+initComponents();
+tablam.addColumn("Producto");
+tablam.addColumn("Precio");
+tablam.addColumn("Cantidad");
+tablam.addColumn("Total");
+jtablecontenido.setModel(tablam);
+}
+private void btnnuevoActionPerformed(java.awt.event.ActionEvent evt) {
+txtcantidad.setText("");
+txtcantidad.requestFocus();
+}
+private void cmbseleccioneItemStateChanged(java.awt.event.ItemEvent evt) {
+int selectedIndex = this.cmbseleccione.getSelectedIndex();
+switch (selectedIndex) {
+case 1:txtprecio.setText("15");
+jlbimagen.setIcon(new ImageIcon("F:/NETBEANS/EXAMEN/imagenes/mouse.jpg"))
+;break;
+case 2:txtprecio.setText("200");
+jlbimagen.setIcon(new ImageIcon("F:/NETBEANS/EXAMEN/imagenes/impresora.jpg"))
+;break;
+case 3:txtprecio.setText("20");
+jlbimagen.setIcon(new ImageIcon("F:/NETBEANS/EXAMEN/imagenes/teclado.jpg"))
+;break;
+case 4:txtprecio.setText("50");
+jlbimagen.setIcon(new ImageIcon("F:/NETBEANS/EXAMEN/imagenes/usb.jpg"))
+;break;
+case 5:txtprecio.setText("80");
+jlbimagen.setIcon(new ImageIcon("F:/NETBEANS/EXAMEN/imagenes/camara.jpg"))
+;break;
+case 6:txtprecio.setText("450");
+jlbimagen.setIcon(new ImageIcon("F:/NETBEANS/EXAMEN/imagenes/monitor.jpg"))
+;break;
+}
+}
+private void btnagregarActionPerformed(java.awt.event.ActionEvent evt) {
+double precio = Integer.parseInt(txtprecio.getText());
+int cantidad = Integer.parseInt(txtcantidad.getText());
+double total; double desc;double prec;
+if (check.isSelected())
+{prec = precio * cantidad;
+desc = prec * 0.15;
+total = prec - desc;}
+else
+{total = precio * cantidad;}
+contotal = contotal + total;
+this.txttotales.setText(contotal +"");
+Vector datos = new Vector();
+datos.addElement(this.cmbseleccione.getSelectedItem());
+datos.addElement(txtprecio.getText());
+datos.addElement(txtcantidad.getText());
+datos.addElement(total +"");
+tablam.addRow(datos);
+jtablecontenido.setModel(tablam);
+txtcontador.setText(tablam.getRowCount()+"");
+}
+private void btnquitarActionPerformed(java.awt.event.ActionEvent evt) {
+int indice = jtablecontenido.getSelectedRow();
+tablam.removeRow(indice);
+jtablecontenido.setModel(tablam);
+}
+private void btnsalirActionPerformed(java.awt.event.ActionEvent evt) {
+if (javax.swing.JOptionPane.showConfirmDialog
+(this,"Esta seguro","Aviso",javax.swing.JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION){
+System.exit(0);
+}
+}
+}
+*/
