8f4013e8bdc688dc758955888a27033061f11c97
Leandro Cuevas <lec.cuevas@gmail.com>
2018-10-10 20:36:35 -0300

    Merge origin/back_end into back_end
    Conflicts:
    	src/daos/FotosDao.java

--- src/daos/FotosDao.java
+++ src/daos/FotosDao.java
@@ -1,24 +1,37 @@
-
 package daos;
 
+import conexion.Conexion;
+import examen_7_album.Foto;
+import examen_7_album.Album;
 import java.sql.*;
 import java.util.*;
 
 
 public class FotosDao {
-    
-    public FotosDao(){
+
+    public FotosDao() {
     }
 
-void seleccionarFotos(){
-    
-    
+    public List<Foto> seleccionarFotos(int idAlbum) throws SQLException {
 
+        ArrayList<Foto> lasFotos = new ArrayList<>();
 
+        Conexion conex = Conexion.getInstance();
+        ResultSet fotosBd = conex.consultarSQL("Select F.* from F.Foto where F.idalbum = " + idAlbum);
+        while(fotosBd.next()){
+            
+            
+            
+            
+            
+        
+        }
+        return lasFotos;
 
-}   
+    }
 
-    
-    public void agregarFoto(){
+    public void agregarFoto() {
+
     }
 }
+//ResultSet rs = Conexion.
