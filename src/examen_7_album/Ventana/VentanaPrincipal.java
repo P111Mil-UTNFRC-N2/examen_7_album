7d9cd99f673a730e9db48ffcf69b063216f1ccb1
Rubén Romero <romeroa.ruben@gmail.com>
2018-10-12 20:39:38 -0300

    Modificaciones grilla Album.

--- src/examen_7_album/Ventana/VentanaPrincipal.java
+++ src/examen_7_album/Ventana/VentanaPrincipal.java
@@ -17,79 +17,82 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author alumno
  */
-public class VentanaPrincipal extends JFrame implements ActionListener{
-    public  JMenuBar barra;
+public class VentanaPrincipal extends JFrame implements ActionListener {
+
+    public JMenuBar barra;
     public JMenu archivo, album;
     public JMenuItem foto, salir, crear, editar;
-   public VentanaPrincipal(){
-       barra=new JMenuBar();
-       archivo=new JMenu("Archivo");
-       foto=new JMenuItem("Foto");
-       salir=new JMenuItem("Salir");
-       crear=new JMenuItem("Crear");
-       editar=new JMenuItem("Editar");
-       album=new JMenu("Album");
-       
-       archivo.add(foto);
-       archivo.add(album);
-       archivo.add(salir);
-       album.add(crear);
-       album.add(editar);
-       
-       barra.add(archivo);
-       setJMenuBar(barra);
-      
-       
-       
-       
-       salir.addActionListener(this);
-      
-       foto.addActionListener(new ActionListener() {
-           @Override
-           public void actionPerformed(ActionEvent ae) {
-               VentanaFoto vf=new VentanaFoto(VentanaPrincipal.this);
-           }
-       });
-       crear.addActionListener(new ActionListener() {
-           @Override
-           public void actionPerformed(ActionEvent ae) {
-               VentanaAlbum va=new VentanaAlbum(VentanaPrincipal.this);
-           }
-       });
-       
-       DefaultTableModel modelo = new DefaultTableModel();
-       
-       
-       modelo.addColumn("Nombre del Album");
-       modelo.addColumn("Nombre del Album2");
-       modelo.addColumn("Nombre del Album3");
-       
-       modelo.addRow(new Object []{"Los wachitos",20,"15/03/14"});       
-       
-       JTable tabla = new JTable();
-       
-       tabla.setModel(modelo);
-       
-       add(tabla);
-       
-       ConfigurarVentana();
-   }
-  public void ConfigurarVentana(){
-      setSize( 500, 300);
-      setLocationRelativeTo(this);
-      setVisible(true);
-      setDefaultCloseOperation(EXIT_ON_CLOSE);
-  }
+    private javax.swing.JScrollPane jScrollPane1;
+
+    public VentanaPrincipal() {
+        barra = new JMenuBar();
+        archivo = new JMenu("Archivo");
+        foto = new JMenuItem("Foto");
+        salir = new JMenuItem("Salir");
+        crear = new JMenuItem("Crear");
+        editar = new JMenuItem("Editar");
+        album = new JMenu("Album");
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+
+        archivo.add(foto);
+        archivo.add(album);
+        archivo.add(salir);
+        album.add(crear);
+        album.add(editar);
+
+        barra.add(archivo);
+        setJMenuBar(barra);
+
+        salir.addActionListener(this);
+
+        foto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                VentanaFoto vf = new VentanaFoto(VentanaPrincipal.this);
+            }
+        });
+        crear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                VentanaAlbum va = new VentanaAlbum(VentanaPrincipal.this);
+            }
+        });
+
+        DefaultTableModel modelo = new DefaultTableModel();
+
+        modelo.addColumn("Nombre del Album");
+        modelo.addColumn("Nombre del Album2");
+        modelo.addColumn("Nombre del Album3");
+
+        modelo.addRow(new Object[]{"Los wachitos", 20, "15/03/14"});
+
+        JTable tabla = new JTable();
+
+        tabla.setModel(modelo);
+        jScrollPane1.setViewportView(tabla);
+        
+        add(jScrollPane1);
+
+        ConfigurarVentana();
+    }
+
+    public void ConfigurarVentana() {
+        setSize(500, 300);
+        setLocationRelativeTo(this);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        if(salir == e.getSource()){
-            
+
+        if (salir == e.getSource()) {
+
             System.exit(0);
-            
+
         }
-        
+
     }
-    
+
 }
